5c8fd96ce13741437f12eb338d171b699697fa41
sbx_user1051 <sbx_user1051@169.254.28.169>
2024-05-29 11:53:46 +0000

    Wed May 29 11:53:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,29 @@
+import re
 
+base_prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15,
+}
+
+discounts = {
+    "A": {
+        "count": 3,
+        "price": 130
+    },
+    "B": {
+        "count": 2,
+        "price": 45
+    }
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    base_prices = {
-        "A": 50,
-        "B": 30,
-        "C": 20,
-        "D": 15,
-    }
+    valid_string_re = re.compile("[ABCD]+")
+    if not valid_string_re.fullmatch(skus):
+        return -1
+
+
+
